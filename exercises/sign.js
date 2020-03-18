94f2a1adf5e0e82df1a89d0c88c751125db2faee
Emily Déat <emily.deat@gmail.com>
2020-03-18 15:58:38 +0100

    resolve sign

--- exercises/sign.js
+++ exercises/sign.js
@@ -11,8 +11,16 @@
  */
 
 // Your code:
-
-//* Begin of tests
+const sign = (a) => {
+        if (a > 0) {
+            return 1
+        } else if (a < 0) {
+            return -1
+        } else {
+            return 0
+        }
+    }
+    //* Begin of tests
 const assert = require('assert');
 
 assert.strictEqual(typeof sign, 'function');
